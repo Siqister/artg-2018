dfa54680016f8a64ea2bcf32f0a43bb14c315772
siqister <siqi.zhu@gmail.com>
2018-02-16 12:08:04 -0500

    Week 6 content

---
+++ 06-1-canvas/src/index.js
@@ -0,0 +1,41 @@
+/***
+  Week 6: Basics of canvas API
+	https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+***/
+
+//6.1 Create canvas element
+//Remember to set width and height explicitly
+
+
+//6.2 Customize fill, stroke, lineWidth
+
+
+//6.3 Draw
+//Primitive shapes: rectangle
+
+
+//Primitive shapes: text
+
+
+//Path : line
+
+
+//Path : arc
+
+
+//Path : circle
+
+
+//Path : curves
+
+
+//Canvas transform
+
+
+//6.4 Drawing multiple path with Path2D
+
+
+//6.5 <canvas> to <svg> using d3.path
+
+
+//6.6 Basic canvas animations
